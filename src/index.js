56aa168a4fd6caa9874b3fc42a1bdd68fc6c2308
Sohail025 <msohail0264@gmail.com>
2024-05-16 12:59:45 +0500

    Restaurant React App Commit 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,112 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CSS from "./index.css";
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Meneu />
+      <Footer />
+    </div>
+  );
+}
+function Header() {
+  return (
+    <main className="Header">
+      <h1>Our Menue</h1>
+    </main>
+  );
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Meneu() {
+  return (
+    <main className="menue">
+      <ul className="pizzas">
+        {pizzaData.map((ob) => {
+          return <Pizza ob={ob} key={ob.name} />;
+        })}
+      </ul>
+    </main>
+  );
+}
+function Footer() {
+  const Hour = new Date().getHours();
+  const openHour = 10;
+  const CloseHour = 11;
+  const isOpen = Hour < CloseHour && Hour > openHour;
+  return (
+    <footer className="footer">
+      {isOpen ? (
+        <Order CloseHour={CloseHour} />
+      ) : (
+        <p>We are closed. Please ComeBack at {openHour}:00.</p>
+      )}
+    </footer>
+  );
+}
+function Order({ CloseHour }) {
+  <div>
+    <p>We are Open untill {CloseHour}:00. Come or Order Online</p>
+    <button className="btn order">Order Now</button>
+  </div>;
+}
+function Pizza({ ob }) {
+  return (
+    <li className={`pizza ${ob.soldOut ? "sold-out" : ""}`}>
+      <div>
+        <img src={ob.photoName} alt={ob.title} />
+        <div>
+          <p>{ob.name}</p>
+          <p>{ob.ingredients}</p>
+          <span>{ob.soldOut ? "SOLD OUT" : `${ob.price}$`}</span>
+        </div>
+      </div>
+    </li>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
